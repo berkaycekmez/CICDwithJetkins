c6f83c1791bc498d5e6d6566ba7861280647df70
berkaycekmez <xiorusss@gmail.com>
2025-05-20 19:15:31 +0300

    init

---
+++ src/main/java/org/example/Controllers/HomeController.java
@@ -0,0 +1,13 @@
+package org.example.Controllers; // Veya daha spesifik bir alt paket, örn. org.example.controller
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String hello() {
+        return "Merhaba Jenkins!";
+    }
+}
